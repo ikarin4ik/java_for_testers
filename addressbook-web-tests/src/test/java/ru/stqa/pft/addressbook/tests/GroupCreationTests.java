11cfc3786abd6161ddd564fd822f6b01cc2543b5
Karina Garmash <karina@garmash.org>
2017-11-11 23:57:37 +0500

    Добавили проверки в тесты: GroupCreationTests, GroupModificationTests, GroupDeletionTests на количество групп до и после изменений в списке. Создали новый метод getGroupCount, который добавили в GroupHelper. Сделали проверки в конце каждого теста, что количество групп в конце теста (при создании новой группы) увеличилось на 1; количество групп в конце теста (при модификации группы) не изменилось; количество групп в конце теста (при удалении группы) уменьшилось на 1.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
@@ -7,9 +8,11 @@ public class GroupCreationTests  extends TestBase {
 
     @Test
     public void testGroupCreation() {
-
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().createGroup(new GroupData("test1", "test2", "test3"));
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
     }
 
 }
