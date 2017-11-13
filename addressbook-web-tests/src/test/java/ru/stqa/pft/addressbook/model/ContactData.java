512a822d4e5833214665c4a48b33ac85f4f43de0
Karina Garmash <karina@garmash.org>
2017-11-14 00:32:04 +0500

    Задание 9 Реализуем проверки путем сравнения списков реализовали проверку списка контактов до/после удаления путем сравнения индексов. В ContactData сгенерировали два метода: toString (который переводит в строку), equals() and hashcode() (помогает сравнивать объекты).

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.addressbook.model;
 public class ContactData {
     private final String firstName;
     private final String lastName;
+
+
     private final String address;
     private final String homephone;
     private final String mobilephone;
@@ -45,5 +47,31 @@ public class ContactData {
 
     public String getGroup() {
         return group;
+    }
+
+    @Override
+    public String toString() {
+        return "ContactData{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ContactData that = (ContactData) o;
+
+        if (firstName != null ? !firstName.equals(that.firstName) : that.firstName != null) return false;
+        return lastName != null ? lastName.equals(that.lastName) : that.lastName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstName != null ? firstName.hashCode() : 0;
+        result = 31 * result + (lastName != null ? lastName.hashCode() : 0);
+        return result;
     }
 }
