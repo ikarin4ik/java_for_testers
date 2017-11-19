85f9b7407e00ffbc7b1087ac36820f6939aef854
Karina Garmash <karina@garmash.org>
2017-11-19 21:49:38 +0500

    Научились писать проверки в одну строчку, используя fluent интерфейс. С помощью библиотеки hamcrest изменили проверки в тестах.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -1,11 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
-import org.testng.Assert;
+import org.hamcrest.CoreMatchers;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
-import java.util.Set;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.testng.Assert.assertEquals;
 
 public class GroupModificationTests extends TestBase {
 
@@ -19,17 +21,15 @@ public class GroupModificationTests extends TestBase {
 
         @Test
         public void testGroupModification() {
-            Set<GroupData> before = app.group().all();
+            Groups before = app.group().all();
             GroupData modifiedGroup = before.iterator().next();
             GroupData group = new GroupData()
                     .withId(modifiedGroup.getId()).withName("test1").withHeader("test2").withFooter("test3");
             app.group().modify(group);
-            Set<GroupData> after = app.group().all();
-            Assert.assertEquals(after.size(), before.size());
+            Groups after = app.group().all();
+            assertEquals(after.size(), before.size());
 
-            before.remove(modifiedGroup);
-            before.add(group);
-            Assert.assertEquals(before, after);
+            assertThat(after, CoreMatchers.equalTo(before.without(modifiedGroup).withAdded(group)));
 
         }
     }
