e1f0f7d3c9415e492b3115d62211f9c02e4c5c94
Karina Garmash <karina@garmash.org>
2017-11-30 00:49:08 +0500

    Задание №14: Вынести описание тестового стенда в конфигурационный файл Реализовали загрузку информации о тестовом стенде из конфигурационного файла. Теперь при запуске тестов из консоли можно явно передвать свойста браузера или указывать конфигурационный файл, в котором содержатся настройки конкретного тестового стенда

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -5,11 +5,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
 
+    private final Properties properties;
     WebDriver wd;
 
     private SessionHelper sessionHelper;
@@ -20,9 +25,14 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
+
     }
 
-    public void init() {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+
         if (Objects.equals(browser, BrowserType.FIREFOX)) {
             System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/stderr");
             wd = new FirefoxDriver();
@@ -30,12 +40,12 @@ public class ApplicationManager {
             wd = new ChromeDriver();
         }
         wd.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
-        wd.get("http://localhost/index.php");
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         sessionHelper = new SessionHelper(wd);
         contactHelper = new ContactHelper(wd);
-        sessionHelper.login("admin", "secret");
+        sessionHelper.login(properties.getProperty("web.adminLogin"), properties.getProperty("web.adminPassword"));
     }
 
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,15 +1,15 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
-import static org.openqa.selenium.remote.BrowserType.CHROME;
-
 
 public class TestBase {
 
-    protected static final ApplicationManager app = new ApplicationManager(CHROME);
+    protected static final ApplicationManager app
+            = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
     @BeforeSuite
     public void setUp() throws Exception {
