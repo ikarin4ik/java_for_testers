3196f30ccacd7fb1e1ca666ef14f96b9ee3ac0c1
Karina Garmash <karina@garmash.org>
2017-12-10 22:54:27 +0500

    Реализовали ленивую инициализацию помощников. Создали тест для регистрации пользователя в баг трекере. При помощи ленивой инициализации откладываем долгие действия на более поздний этап (или не выполняем их, если не понадобятся)

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -15,9 +15,10 @@ import java.util.concurrent.TimeUnit;
 public class ApplicationManager {
 
     private final Properties properties;
-    WebDriver wd;
+    private WebDriver wd;
 
     private String browser;
+    private RegistrationHelper registrationHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -28,20 +29,13 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
-
-        if (Objects.equals(browser, BrowserType.FIREFOX)) {
-            System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/stderr");
-            wd = new FirefoxDriver();
-        } else if (Objects.equals(browser, BrowserType.CHROME)) {
-            wd = new ChromeDriver();
-        }
-        wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-        wd.get(properties.getProperty("web.baseUrl"));
     }
 
     public void stop() {
+        if (wd != null) {
             wd.quit();
         }
+    }
 
     public HttpSession newSession() {
         return new HttpSession(this);
@@ -50,4 +44,26 @@ public class ApplicationManager {
     public String getProperty(String key) {
         return properties.getProperty(key);
     }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper =  new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if (Objects.equals(browser, BrowserType.FIREFOX)) {
+                System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/stderr");
+                wd = new FirefoxDriver();
+            } else if (Objects.equals(browser, BrowserType.CHROME)) {
+                wd = new ChromeDriver();
+            }
+            wd.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
+    }
+
 }
