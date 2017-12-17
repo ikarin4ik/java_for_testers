79ebb146c0a06a8f6af50f3f5509052c0e63b770
Karina Garmash <karina@garmash.org>
2017-12-17 21:13:13 +0500

    НАучились по описанию сервиса в формате wsdl генерировать код на языке java, который может быть использован для взаимодействия с удаленным сервисом (SOAP)

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,16 @@
+package ru.stqa.pft.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class GeoIpServiceTests {
+
+    @Test
+    public void testMyIp() {
+        GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("46.101.138.63");
+        assertEquals(geoIP.getCountryCode(), "RUS");
+    }
+}
